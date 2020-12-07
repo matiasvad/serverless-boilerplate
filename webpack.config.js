61c5f8531e36ee68873add950421cb253baed73f
Matias Vad <matiasvad@gmail.com>
2020-12-07 14:46:22 +0100

    indents
    Signed-off-by: Matias Vad <matiasvad@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,18 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: {
-    'app.functions/index': './src/app.functions/index.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        { from: './src/app.functions', to: 'app.functions', toType: 'dir' },
-      ],
-    }),
-  ],
+	entry: {
+		'app.functions/index': './src/app.functions/index.js',
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].bundle.js',
+	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [
+				{ from: './src/app.functions', to: 'app.functions', toType: 'dir' },
+			],
+		}),
+	],
 };
